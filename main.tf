47d1d46d1382035a7052082fc947020af5a27ad3
devopstani <devopstani@gmail.com>
2025-11-25 11:24:12 -0800

    updated rg

--- main.tf
+++ main.tf
@@ -1,24 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name_prefix}-rg01-${random_integer.rg_suffix.result}"
+  name     = "${var.resource_group_name_prefix}-rg01"
   location = var.resource_group_location
   tags     = var.tags
 }
 
-resource "random_integer" "rg_suffix" {
-  min = 2
-  max = 3
-}
-
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
-}
-output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
-}
-output "resource_group_location" {
-  value = azurerm_resource_group.rg.location
-}
-output "resource_group_tags" {
-  value = azurerm_resource_group.rg.tags
 }
 
