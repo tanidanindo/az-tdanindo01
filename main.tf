d8510ab98728313f6ba38f1a069bad60a6b26eff
devopstani <devopstani@gmail.com>
2025-11-25 16:42:15 -0800

    added law

--- main.tf
+++ main.tf
@@ -8,3 +8,13 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+resource "azurerm_log_analytics_workspace" "tanilaw01" {
+  # Configuration details
+  name                = "tanilaw01" # Must be globally unique
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Free" # Pricing Tier (e.g., Free, PerGB2018, Consumption, etc.)
+
+  # Retention setting (data older than this is purged)
+  retention_in_days = 30
+}
